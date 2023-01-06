61afc7fccd663b7a6f1c47afcd3eb2d1271d6330
조성동 <racoCSD@gmail.com>
2023-01-06 11:04:27 +0900

    Cart DTO 수정

--- src/com/team1/jogiyo/cart/Cart.java
+++ src/com/team1/jogiyo/cart/Cart.java
@@ -1,4 +1,7 @@
 package com.team1.jogiyo.cart;
+
+import com.team1.jogiyo.product.Product;
+
 /*
  CREATE TABLE cart(
 		c_no                          		NUMBER(10)		 NULL ,
@@ -11,64 +14,58 @@ public class Cart {
 	private int c_no;
 	private int c_qty;
 	/*FK*/
-	private int p_no;
 	private String m_id;
+	//private int p_no;
+	private Product product;
 	
-	
-	public Cart(int c_no, int c_qty, int p_no, String m_id) {
+	public Cart() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Cart(int c_no, int c_qty, String m_id, Product product) {
 		super();
 		this.c_no = c_no;
 		this.c_qty = c_qty;
-		this.p_no = p_no;
 		this.m_id = m_id;
+		this.product = product;
 	}
-
 
 	public int getC_no() {
 		return c_no;
 	}
 
-
 	public void setC_no(int c_no) {
 		this.c_no = c_no;
 	}
-
 
 	public int getC_qty() {
 		return c_qty;
 	}
 
-
 	public void setC_qty(int c_qty) {
 		this.c_qty = c_qty;
 	}
-
-
-	public int getP_no() {
-		return p_no;
-	}
-
-
-	public void setP_no(int p_no) {
-		this.p_no = p_no;
-	}
-
 
 	public String getM_id() {
 		return m_id;
 	}
 
-
 	public void setM_id(String m_id) {
 		this.m_id = m_id;
 	}
 
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
 
 	@Override
 	public String toString() {
-		return "Cart [c_no=" + c_no + ", c_qty=" + c_qty + ", p_no=" + p_no + ", m_id=" + m_id + "]";
+		return "Cart [c_no=" + c_no + ", c_qty=" + c_qty + ", m_id=" + m_id + ", product=" + product + "]";
 	}
-	
 	
 	
 }
