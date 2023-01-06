d01ae8012bb9028d08309fdbc92fe282f93896f0
yuuuuna0 <jyn9306@gmail.com>
2023-01-06 17:33:49 +0900

    ..

--- src/com/team1/jogiyo/cart/CartInterface.java
+++ src/com/team1/jogiyo/cart/CartInterface.java
@@ -7,6 +7,6 @@ public interface CartInterface {
 	public int insert(Cart cart) throws Exception;
 	public int update(Cart cart) throws Exception;
 	public int delete(int c_no) throws Exception;
-	public Cart findByPrimaryKey(int c_no) throws Exception;
-	public List<Cart> findAll() throws Exception;
+	public List<Cart> findByUser() throws Exception;
+	public List<Cart> findByCartNo() throws Exception;
 }
