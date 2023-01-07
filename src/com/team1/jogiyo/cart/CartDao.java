0794fba23bd7cb4e1bb0d1d3d8c4ed51398b8ff5
jeong-yoona <jeong-yoona@localhost>
2023-01-07 11:34:39 +0900

    ..

--- src/com/team1/jogiyo/cart/CartDao.java
+++ src/com/team1/jogiyo/cart/CartDao.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import com.team1.jogiyo.common.DataSource;
 
-public class CartDao implements CartInterface{
+public class CartDao{
 	Cart cart=new Cart();
 	private DataSource dataSource;
 	
@@ -14,44 +14,57 @@ public class CartDao implements CartInterface{
 		this.dataSource=new DataSource();
 	}
 
-	@Override
-	public int insert(Cart cart) throws Exception {
 	//카트에 추가
+	public int insert(Cart cart) throws Exception {
 		Connection con=dataSource.getConnection();
 		PreparedStatement pstmt=con.prepareStatement(CartSQL.CART_INSERT);
-		pstmt.setInt(0, cart.getC_qty());
-		pstmt.setString(1, cart.getM_id());
-		pstmt.setObject(2, cart.getProduct());
+		pstmt.setInt(1, cart.getC_qty());
+		pstmt.setString(2, cart.getM_id());
+		pstmt.setObject(3, cart.getProduct());
 		int rowCount=pstmt.executeUpdate();
 		
 		return rowCount;
 	}
 
-	@Override
+	//카트에 있는 제품의 수량 변경 (카트리스트에서 변경)
 	public int update(Cart cart) throws Exception {
-	//카트에 있는 제품의 수량 설정
 		Connection con=dataSource.getConnection();
 		PreparedStatement pstmt=con.prepareStatement(CartSQL.CART_UPDATE_BY_C_NO);
-		pstmt.setInt(0, cart.getC_qty());
-		pstmt.setInt(1, cart.getC_no());
+		pstmt.setInt(1, cart.getC_qty());
+		pstmt.setInt(2, cart.getC_no());
 		int rowCount=pstmt.executeUpdate();
 		
 		return rowCount;
 	}
 
-	@Override
-	public int delete(int c_no) throws Exception {
-		return 0;
+	//카트 물품 하나 삭제 
+	public int deleteByCartNo(int c_no) throws Exception {
+		Connection con=dataSource.getConnection();
+		PreparedStatement pstmt=con.prepareStatement(CartSQL.CART_DELETE_BY_C_NO);
+		pstmt.setInt(1, c_no);
+		int rowCount=pstmt.executeUpdate();
+
+		return rowCount;
 	}
 
+	//카트 전체 삭제
+	public int deleteAll(String m_id) throws Exception {
+		Connection con=dataSource.getConnection();
+		PreparedStatement pstmt=con.prepareStatement(CartSQL.CART_DELETE_BY_M_ID);
+		pstmt.setString(1, m_id);
+		int rowCount=pstmt.executeUpdate();
 
-	@Override
+		return rowCount;
+	}
+
 	public List<Cart> findByUser() throws Exception {
+		Connection con=dataSource.getConnection();
+		PreparedStatement pstmt=con.prepareStatement(CartSQL.CART_FIND_BY_M_ID);
+		
 		return null;
 	}
 	
 	
-	@Override
 	public List<Cart> findByCartNo() throws Exception {
 		return null;
 	}
