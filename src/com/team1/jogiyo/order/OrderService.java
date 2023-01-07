23ac74bdbaae6b735a2b348d6a1a76dd9a3db9df
chosh <racoCSD@gmail.com>
2023-01-07 19:30:55 +0900

    업데이트 OrderService

--- src/com/team1/jogiyo/order/OrderService.java
+++ src/com/team1/jogiyo/order/OrderService.java
@@ -1,6 +1,7 @@
 package com.team1.jogiyo.order;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.team1.jogiyo.cart.Cart;
 import com.team1.jogiyo.cart.CartDao;
@@ -44,25 +45,45 @@ public class OrderService {
 	/*
 	 * 상품에서 직접주문
 	 */
-//	public int create(String sUserId,int p_no,int oi_qty) throws Exception{
-//		//상품(product)객체에서 정보 orderItem객체로 전이 필요
-//		Product product=productDao.findByPrimaryKey(p_no);
-//		OrderItem orderItem=new OrderItem(0, oi_qty, p_no, product);
-//		ArrayList<OrderItem> orderItemList=new ArrayList<OrderItem>();
-//		
-//		
-//	}
-//	/*
-//	 * cart에서 주문
-//	 */
-//	public int create(String sUserId) throws Exception{
-//		
-//	}
-//	/*
-//	 * cart에서 선택주문
-//	 */
-//	public int create(String sUserId,String[] cart_item_noStr_array) throws Exception{
-//		
-//	}
+	public int create(String sUserId,int p_no,int oi_qty) throws Exception{
+		//상품(product)객체에서 정보 orderItem객체로 전이 필요
+		Product product=productDao.findByPrimaryKey(p_no);
+		OrderItem orderItem=new OrderItem(0, oi_qty, p_no, product);
+		// Orders 객체에 List<OrderItem> 필요
+		ArrayList<OrderItem> orderItemList=new ArrayList<OrderItem>();
+		
+		Orders newOrders=new Orders(0, 
+									null, 
+									orderItemList.get(0).getOi_qty()*orderItemList.get(0).getProduct().getP_price(), 
+									sUserId);
+		newOrders.setOrderItemList(orderItemList);
+		return orderDao.insert(newOrders);
+	}
+	/*
+	 * cart에서 주문
+	 */
+	public int create(String sUserId) throws Exception{
+		//카트(cart)객체에서 정보 orderItem객체로 모두 전이
+		List<Cart> cartList = cartDao.findByUser(sUserId);
+		ArrayList<OrderItem> orderItemList=new ArrayList<OrderItem>();
+		int o_tot_price=0;
+		for (Cart cart : cartList) {
+			//Cart -> OrderItem 정보 전이
+			OrderItem orderItem=new OrderItem(0, cart.getC_qty(), 0, cart.getProduct());
+			orderItemList.add(orderItem);
+			o_tot_price+=orderItem.getOi_qty()*orderItem.getProduct().getP_price();
+		}
+		Orders newOrders=new Orders(0, null, o_tot_price, sUserId);
+		newOrders.setOrderItemList(orderItemList);
+		orderDao.insert(newOrders);
+		cartDao.deleteAll(sUserId);
+		return 0;
+	}
+	/*
+	 * cart에서 선택주문
+	 */
+	public int create(String sUserId,String[] cart_item_noStr_array) throws Exception{
+		return 0;
+	}
 	
 }
