3b51d5f7cdd60f528cbe1b43e88995305397ffae
user <user@DESKTOP-U4NJ2NO>
2023-01-08 21:50:38 +0900

    ..

--- src/com/team1/jogiyo/product/test/ProductDaoTestMain.java
+++ src/com/team1/jogiyo/product/test/ProductDaoTestMain.java
@@ -9,7 +9,7 @@ public class ProductDaoTestMain {
 
 		//상품 추가
 		ProductDao productDao = new ProductDao();
-		int result = productDao.insert(new Product(12,"떡떡볶이","aaaa.jpg",4500,"왕맛있는떡볶이",30));
+		int result = productDao.insert(new Product(0,"떡떡볶이","aaaa.jpg",4500,"왕맛있는떡볶이",30));
 		System.out.println("상품추가 : " + result );
 		
 		//상품 수정
@@ -18,15 +18,15 @@ public class ProductDaoTestMain {
 		//상품 삭제
 		productDao.delete("떡떡볶이");		
 		
-		//상품 검색(번호)
+		//상품 보기(번호)
 		System.out.println(productDao.findByPrimaryKey(10));
 		
 		
-		//상품 검색(이름)
+		//상품 보기(이름)
 		System.out.println(productDao.findByName("찰순대"));
 		
 		
-		//상품 검색(전체)
+		//상품 보기(전체)
 		System.out.println(productDao.findAll());
 		
 	}
---
+++ src/com/team1/jogiyo/product/test/ProductServiceTestMain.java
@@ -0,0 +1,34 @@
+package com.team1.jogiyo.product.test;
+
+import com.team1.jogiyo.product.Product;
+import com.team1.jogiyo.product.ProductService;
+
+public class ProductServiceTestMain {
+
+	public static void main(String[] args) throws Exception {
+	ProductService productService = new ProductService();
+		
+	//상품 추가
+	Product insert = new Product(0,"김치찜","image2.jpg",21000,"설명",10);	
+	productService.insert(insert);
+
+	//상품 삭제
+	productService.delete("치킨");
+	
+	//상품 보기 (번호)
+	productService.findByPrimaryKey(2);
+	
+	//상품 보기 (이름)
+	productService.findByName("김치찜");
+
+	//상품 찾기 (전체)
+	productService.findAll();
+	
+	//상품 수정
+	Product update = productService.findByPrimaryKey(2);
+	update.setP_desc("상세설명~");
+	update.setP_price(22000);
+
+	}
+
+}
