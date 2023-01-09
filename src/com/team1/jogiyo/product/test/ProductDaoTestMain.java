cb221f8322b732e717e5b1d618a6967f766745ee
leeeeeunji <qwas2555@naver.com>
2023-01-09 09:36:37 +0900

    .

--- src/com/team1/jogiyo/product/test/ProductDaoTestMain.java
+++ src/com/team1/jogiyo/product/test/ProductDaoTestMain.java
@@ -9,14 +9,16 @@ public class ProductDaoTestMain {
 
 		//상품 추가
 		ProductDao productDao = new ProductDao();
-		int result = productDao.insert(new Product(0,"떡떡볶이","aaaa.jpg",4500,"왕맛있는떡볶이",30));
-		System.out.println("상품추가 : " + result );
+		int rowCount = productDao.insert(new Product(0,"떡떡볶이","aaaa.jpg",4500,"왕맛있는떡볶이",30));
+		System.out.println(rowCount + "개 추가");
 		
 		//상품 수정
-		productDao.update(new Product(11,"찰순대","bbb.jpg",4000,"순대맛있",30));
+		rowCount = productDao.update(new Product(11,"찰순대","bbb.jpg",4000,"순대맛있",30));
+		System.out.println(rowCount + "개 수정");
 		
 		//상품 삭제
-		productDao.delete("떡떡볶이");		
+		rowCount = productDao.delete("떡떡볶이");
+		System.out.println(rowCount + "개 삭제");
 		
 		//상품 보기(번호)
 		System.out.println(productDao.findByPrimaryKey(10));
